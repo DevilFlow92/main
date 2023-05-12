31217184db511827bbeff52596487511b5bb1739
DevilFlow92 <loref92@gmail.com>
2023-05-12 23:55:58 +0200

    refresh repository

--- QUERY SALVATE/2017-12-05 query da provare.sql
+++
@@ -1,8 +0,0 @@
-use CLC
-
-select * from T_Documento
-			join T_Incarico on T_Documento.IdIncarico = T_Incarico.IdIncarico
- where DescrizioneCEI like '%sottoscrizione iniziale%' and T_Incarico.CodTipoIncarico = 84
-
-select * from D_TipoIncarico where Descrizione like '%switch fondi%'
---84	Switch FONDI Investimento
