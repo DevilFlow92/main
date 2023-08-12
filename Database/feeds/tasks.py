b97860f49eadd3654ff30fa4dafb9af1b3138737
DevilFlow92 <loref92@gmail.com>
2023-08-12 19:59:07 +0200

    ingest feed with meta in progress

--- Database/feeds/tasks.py
+++ Database/feeds/tasks.py
@@ -1,28 +1,16 @@
-import json
-import os
-import re
-from collections import namedtuple
-from datetime import date, datetime, timedelta
-from decimal import Decimal as D
-from typing import Any, Iterable, Optional, Set, Union, cast
+from datetime import datetime
+from typing import Any, Optional
 
 import pendulum
-import pymysql
 
 from jobs.fromsecret import authdb
-from jobs import ReadCsv, ReadCBI, ReadFixedWidth #,ReadExcel
 from types_db import (
     BusinessValueError,
-    AdditionalParams,
     DataRow,
-    DBObject,
-    ExtParams,
-    MessageState,
     TaskData
 )
-from validation import Schema, SchemaError, FieldRule, RuleDict
-
-from .utils import _file_date, _file_time
+from validation import SchemaError
+from .utils import _file_date
 
 tz_rome = pendulum.timezone("Europe/Rome")  # type: ignore
 
@@ -103,10 +91,12 @@ class MassiveInsert:
     def open(self) -> None:
         self.dbconn, self.cursor = authdb(self.db)
 
+    def close(self) -> None:
+        self.dbconn.close()
+
     def _write_row(self, values: DataRow) -> None:
         table = self.table
-        col_names = f"([{'],['.join(values.keys())}])"
-        col_names = col_names.replace('[','').replace(']','')
+        col_names = f"([{','.join(values.keys())})"
         placeholders = f"VALUES ({','.join(['%s' for _ in values.values()])})"
         _values = [values[c] for c in values]
         insert_statement = f"INSERT INTO {table} {col_names} {placeholders}"
@@ -134,11 +124,6 @@ class MassiveInsert:
         finally:
             self.close()
 
-    def close(self) -> None:
-        self.dbconn.close()
-    
-
-'''
 class IngestFeed:
     """Salva dati letti da una sorgente su db.
 
@@ -160,15 +145,15 @@ class IngestFeed:
         self,
         db: str,
         table: str,
+        time_rx: str | None = None,
         provider: str = None,
-        feed: str = None,
         idetl: int = None,   
         overwrite: bool = False,    
         **kwargs: Any,
     ) -> None:
         self.db = db
         self.provider = provider
-        self.feed = feed
+        self.time_rx = time_rx
         self.table = table
         self.idetl = idetl
         self.overwrite = overwrite
@@ -177,13 +162,121 @@ class IngestFeed:
     def open(self) -> None:
         self.dbconn, self.cursor = authdb(self.db)
 
-    def _write_row(
-            self,
-            meta_id: int,
+    def close(self) ->None:
+        self.dbconn.close()
 
-    )
+    def log_meta(self, source_path:str, pull_ts: datetime) -> tuple[int,bool]:
+        fname = str(source_path).split("@")[0]
+        self.cursor.execute(
+        "SELECT id, ingested, ingest_ts FROM e_meta WHERE FileSorgente like %s"
+        ,f"{fname}%"
+        )
+        rs = self.cursor.fetchone()
+        if rs:
+            if self.overwrite:
+                self.cursor.execute(
+                    "UPDATE e_meta SET FileSorgente = %s, pull_ts = %s ingest_ts = %s WHERE id = %s",
+                    str(source_path),
+                    pull_ts,
+                    datetime.now(),
+                    rs.id
+                )
+            
+            self.dbconn.commit()
+            return rs.id, True
+        
+        file_date = _file_date(str(source_path))
 
-    def run(self, data:TaskData, file_date:str) -> None:
-        if self.overwrite is True:
-            query = f'UPDATE %s set FlagAttivo = 0 WHERE '
-'''
+        col_names = "(FileSorgente, DataFile, Ingest_ts, Pull_ts, Fornitore, IdETL)"
+        placeholders = "VALUES(%s,%s,%s,%s,%s,%s)"
+        insert_statement = f"INSERT INTO e_meta {col_names} {placeholders}"
+        
+        self.cursor.execute(
+            insert_statement,
+            str(source_path),
+            file_date,
+            datetime.now(),
+            pull_ts,
+            self.provider,
+            self.feed,
+            self.idetl,
+        )
+        meta_id = self.cursor.fetchone()[0]
+        self.dbconn.commit()
+        return meta_id, False
+    
+    def delete_row(self, meta_id:int) -> None:
+        table = self.table
+        delete_statement = f"DELETE FROM {table} WHERE meta_id = %s"
+        self.cursor.execute(
+            delete_statement,
+            meta_id
+        )
+            
+    def _source_path_and_ts(self, data: TaskData, source_path: Optional[str]) -> tuple[str, datetime]:
+        if not source_path and "source_path" not in data["meta"]:
+            raise ValueError(
+                "Il source file per i dati deve; o essere presente nel meta di TaskData "
+                "o specificato / overridato come argomento"
+            )
+
+        if source_path:
+            _file_date(source_path)
+            pull_ts = datetime.now()
+        else:
+            source_path = str(data["meta"]["source_path"])
+            pull_ts = datetime.strptime(data["meta"]["timestamp"], "%Y-%m-%dT%H:%M:%S+00:00")
+
+        return (source_path, pull_ts)
+
+    def _write_row(self, meta_id: int, values: DataRow) -> None:
+        table = self.table
+        values["meta_id"] = meta_id
+        col_names = f"({','.join(values.keys())})"
+        placeholders = f"VALUES ({','.join(['%s' for _ in values.values()])})"
+        _values = [values[c] for c in values]
+        insert_statement = f"INSERT INTO {table} {col_names} {placeholders}"
+        self.cursor.execute(insert_statement, _values)
+    
+    def write_row(
+            self, msg_: DataRow, task_meta: dict[str, Any],
+            is_ingested: bool, meta_id: int
+    ) -> None:
+        msg = msg_.copy()
+        try:
+            if is_ingested and self.overwrite:
+                self.delete_row(meta_id)
+            self._write_row(
+                meta_id,
+                msg,
+            )
+        except SchemaError as err:
+            error_msg = str(err).replace("\n", " ")
+            print(error_msg)
+
+    def run(self, data:TaskData,file_date:str, source_path: str | None = None, overwrite: bool | None = None) -> None:
+        task_meta = data["meta"].copy()
+        task_meta["overridden_source_path"] = source_path
+        source_path, pull_ts = self._source_path_and_ts(data, source_path)
+        self.overwrite = overwrite if overwrite is not None else self.overwrite
+        try:
+            self.open()
+            meta_id, is_ingested = self.log_meta(
+                source_path,
+                pull_ts,
+                data["meta"]["sheet"] if "sheet" in data["meta"] and data["meta"]["sheet"] else "-",
+            )
+            if not is_ingested or (is_ingested and self.overwrite):
+                for msg_ in data["data"]:
+                    self.write_row(msg_, source_path, task_meta, is_ingested, meta_id)
+            else:
+                print(f"Skippo file già salvato: {source_path}")
+            self.dbconn.commit()
+
+        except Exception as exc:
+            self.dbconn.rollback()
+            print(f"Errore inatteso {exc.__class__} : {exc}. Processando il file (meta id): {meta_id}")
+            raise exc
+        finally:
+            self.close()
+
